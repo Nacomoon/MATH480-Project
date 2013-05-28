87d15d99c7e728acdfffa0bc1e6b0c5499edb9d7
Nacomoon <genericgaze@gmail.com>
2013-05-28 00:46:13 -0700

    added comments to tableau

--- tableau.py
+++ tableau.py
@@ -1,6 +1,55 @@
-from sage.structure.sage_object import SageObject
+class Tableau(object):
 
-class Tableau(SageObject):
+    r"""
+    
+    I added _latex_ and _matrix_ functions so that you can use latex(a) and matrix(a)
+    with a being your tableau object.
+
+    INPUT:
+
+    Takes in list of symbolic expressions representing the constraints of a linear
+    programming problem. And a single symbolic expression for the objective
+    function of the problem. Use var() to define your variables.
+
+    ``A`` - A list of symbolic expressions, the contraints of the LP.
+
+    ``z`` - A single symbolic expression, the objective function of the LP.
+
+    OUTPUT
+
+    By default it spouts out the tableau as a matrix. Because the set() and
+    union() functions return lists with unexpected orderings, I decided
+    to save a list with the variables of linear program in the same order
+    as they appear in the tableau.
+
+    ``tab`` - The tableau as a list of lists. Each element of the outer elements
+    represents a row in the tableau.
+
+    ``var`` - A list of symbolic variables to help determine the order of the 
+    variables from the linear program in the tableau.
+
+    EXAMPLES
+    
+    sage: load("tableau.py")
+    sage: var('x1,x2,x3');
+    sage: y = [3 * x1 + 2 * x2 - x3 <= 5, 2 * x1 - 2 * x2 <= 4, x3 <= 5];
+    sage: z = 2 * x1 + 3*x2 + 4*x3;
+    sage: asd = Tableau(y,z)
+    [ 3 -1  2  1  0  0  5]
+    [ 2  0 -2  0  1  0  4]
+    [ 0  1  0  0  0  1  5]
+    [-2 -4 -3  0  0  0  0]
+    sage: asd.tab
+    [[3, -1, 2, 1, 0, 0, 5],
+     [2, 0, -2, 0, 1, 0, 4],
+     [0, 1, 0, 0, 0, 1, 5],
+     [-2, -4, -3, 0, 0, 0, 0]]
+    sage: asd.var
+    [x1, x3, x2]
+
+    Notice how asd.var isn't x1,x2,x3 as would be expected.
+    """
+
     tab = []
     var = []
 
@@ -11,10 +60,10 @@ class Tableau(SageObject):
             variables = union(variables,A[i].args())
         variables = union(variables,z.args())
         v1 = len(variables)
-        for i in range(len(T)): #this makes a tableux of all zeros and b
+        for i in range(len(T)): #this makes a matrix of all zeros and b
             for j in range(v1 + len(A)):
                 T[i].append(0)
-        for i in range(len(T) - 1): #this fills in the rest of the tableux
+        for i in range(len(T) - 1): #this fills in all of the zeros
             T[i].append(A[i].right())
             T[i][v1 + i] = 1
             for j in range(v1):
@@ -29,11 +78,13 @@ class Tableau(SageObject):
         T[len(T) - 1].append(0)
         self.tab = T
         self.var = variables
+        print self
+        
     
     def _latex_(self):
         return latex(self._matrix_())
 
-    def _repr_(self):
+    def __repr__(self):
         return self._matrix_().str()
 
     def _matrix_(self):
